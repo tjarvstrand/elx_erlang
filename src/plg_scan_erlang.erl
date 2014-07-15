ed5605ca0bfad8fd38286c6b0df6093a8fa8a16f
Thomas Järvstrand <tjarvstrand@gmail.com>
2014-07-15 16:32:43 +0200

    Add rules and tests for keywords, floats, integers, characters, strings, atoms and variables

--- src/plg_scan_erlang.erl
+++ src/plg_scan_erlang.erl
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% @doc
+%%% @doc Erlang grammar for parse_lib_scan
 %%% @end
 %%% @author Thomas Järvstrand <tjarvstrand@gmail.com>
 %%% @copyright
@@ -27,8 +27,13 @@
 
 %%%_* Exports ==================================================================
 -export([grammar/0,
-
-        atom/3]).
+         atom/4,
+         character/4,
+         float/4,
+         keyword/4,
+         integer/4,
+         string/4,
+         variable/4]).
 
 %%%_* Includes =================================================================
 
@@ -36,34 +41,141 @@
 
 %%%_* Defines ==================================================================
 
+-define(ERLANG_KEYWORDS, ["after",
+                          "and",
+                          "andalso",
+                          "band",
+                          "begin",
+                          "bnot",
+                          "bor",
+                          "bsl",
+                          "bsr",
+                          "bxor",
+                          "case",
+                          "catch",
+                          "cond",
+                          "div",
+                          "end",
+                          "fun",
+                          "if",
+                          "let",
+                          "not",
+                          "of",
+                          "or",
+                          "orelse",
+                          "receive",
+                          "rem",
+                          "try",
+                          "when",
+                          "xor"]).
+
+-define(ERLANG_INTEGER_BASE_MAX, 32).
+
 %%%_* Types ====================================================================
 
-%%%_* API ======================================================================
+%%%_* Grammar ==================================================================
 
 grammar() ->
-    [{"[a-z][a-zA-Z0-9_@]*", fun atom/3}].
+    [{?ERLANG_KEYWORDS,                              fun keyword/4},
+     {"(?:-|\\+)?[0-9]+\\.[0-9]+(e(-|\\+)?[0-9]+)?", fun float/4},
+     {"(?<sign>-|\\+)?"
+      "((?<base>[0-9]+)(#))?"
+      "(?<value>[0-9]+)",                            fun integer/4},
+     {{["\\$\\\\(?<octal>[0-7]?[0-7]?[0-7])",
+        "\\$\\\\\\^(?<control>[A-Za-z])",
+        "\\$\\\\(?<escape>[bdefnrstv])",
+        "\\$\\\\?(?<normal>.)"],
+       [dotall]},                                    fun character/4},
+     {"\"(.*\")*.*\"",                               fun string/4},
+     {["[a-z][a-zA-Z0-9_@]*", "'(.*')*.*'"],         fun atom/4},
+     {"[A-Z_][a-zA-Z0-9_]*",                         fun variable/4}].
+
+%%%_* Token parsers ============================================================
+
+
+atom(Chars, _Matches, Start, End) ->
+  Term = list_to_atom(string:strip(Chars, both, $')),
+  {token,
+   parse_lib_scan:token(atom, Term, Chars, Start, End)}.
+
+character(Chars, Matches, Start, End) ->
+  {Group, MatchStr} = character_match(Chars, Matches),
+  Term              = character_term(Group, MatchStr),
+  {token, parse_lib_scan:token(character, Term, Chars, Start, End)}.
+
+float(Chars, _Matches, Start, End) ->
+  {token,
+   parse_lib_scan:token(float, list_to_float(Chars), Chars, Start, End)}.
+
+integer(Chars, Matches, Start, End) ->
+  try do_integer(Chars, Matches, Start, End)
+  catch
+    error:{illegal_base, _} = Rsn -> {error, Rsn};
+    error:badarg                  -> {error, illegal_integer}
+  end.
+
+keyword(Chars, _Matches, Start, End) ->
+  {token,
+   parse_lib_scan:token(keyword, list_to_atom(Chars), Chars, Start, End)}.
+
+variable(Chars, _Matches, Start, End) ->
+  {token,
+   parse_lib_scan:token(variable, list_to_atom(Chars), Chars, Start, End)}.
+
+string(Chars, _Matches, Start, End) ->
+  Term = string:strip(Chars, both, $"),
+  {token,
+   parse_lib_scan:token(string, Term, Chars, Start, End)}.
+
 
 %%%_* Internal functions =======================================================
 
-atom(Chars, Start, End) ->
-    {token, parse_lib_scan:token(list_to_atom(Chars), Chars, Start, End)}.
+character_match(Str, Matches) ->
+  character_match(Str, Matches, [normal, escape, control, octal]).
+
+character_match(Str, Matches, [Group|Groups]) ->
+  io:fwrite(user, <<"~p ~p ~p: Matches = ~p~n">>, [self(), ?MODULE, ?LINE, Matches]),
+  case parse_lib_scan:match_named_group_string(Str, Group, Matches) of
+    {error, nomatch}  -> character_match(Str, Matches, Groups);
+    {ok,    MatchStr} -> {Group, MatchStr}
+  end.
+
+character_term(normal,  [C])       -> C;
+character_term(control, [C])       -> C band 31;
+character_term(escape,  "b")       -> $\b;
+character_term(escape,  "d")       -> $\d;
+character_term(escape,  "e")       -> $\e;
+character_term(escape,  "f")       -> $\f;
+character_term(escape,  "n")       -> $\n;
+character_term(escape,  "r")       -> $\r;
+character_term(escape,  "s")       -> $\s;
+character_term(escape,  "t")       -> $\t;
+character_term(escape,  "v")       -> $\v;
+character_term(octal,   [X, Y, Z]) -> (X * 64) + (Y * 8) + Z - ($0 * 8).
+
+
+do_integer(Chars, Matches, Start, End) ->
+  SignStr =
+    case parse_lib_scan:match_named_group_string(Chars, sign, Matches) of
+      {error, nomatch} -> "";
+      {ok, SignStr0} -> SignStr0
+    end,
+  Base = case parse_lib_scan:match_named_group_string(Chars, base, Matches) of
+           {error, nomatch} -> 10;
+           {ok, BaseStr}    ->
+             case list_to_integer(BaseStr) of
+               Base0 when Base0 > ?ERLANG_INTEGER_BASE_MAX ->
+                 erlang:error({illegal_base, Base0});
+               Base0 ->
+                 Base0
+             end
+         end,
+  {ok, ValueStr} = parse_lib_scan:match_named_group_string(Chars, value, Matches),
+  Term = list_to_integer(SignStr ++ ValueStr, Base),
+  {token, parse_lib_scan:token(integer, Term, Chars, Start, End)}.
+
 
 %%%_* Tests ====================================================================
-
-grammar_symbol_test_() ->
-    [ ?_assertEqual({ok,
-                     [parse_lib_scan:token(a,
-                                           "a",
-                                           parse_lib_scan:point(1, 1, 1),
-                                           parse_lib_scan:point(1, 1, 1))]},
-                    parse_lib_scan:string("a", grammar())),
-      ?_assertEqual({ok,
-                     [parse_lib_scan:token('and',
-                                           "and",
-                                           parse_lib_scan:point(1, 1, 1),
-                                           parse_lib_scan:point(3, 1, 3))]},
-                    parse_lib_scan:string("and", grammar()))
-    ].
 
 %%%_* Test helpers =============================================================
 
---
+++ test/plg_scan_erlang_tests.erl
@@ -0,0 +1,120 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc Tests for plg_scan_erlang
+%%% @end
+%%% @author Thomas Järvstrand <tjarvstrand@gmail.com>
+%%% @copyright
+%%% Copyright 2012 Thomas Järvstrand <tjarvstrand@gmail.com>
+%%%
+%%% This file is part of parse_lib_grammars.
+%%%
+%%% parse_lib_grammars is free software: you can redistribute it and/or modify
+%%% it under the terms of the GNU General Public License as published by
+%%% the Free Software Foundation, either version 3 of the License, or
+%%% (at your option) any later version.
+%%%
+%%% parse_lib_grammars is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%% GNU General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU General Public License
+%%% along with parse_lib_grammars. If not, see <http://www.gnu.org/licenses/>.
+%%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%_* Module declaration =======================================================
+-module(plg_scan_erlang_tests).
+
+%%%_* Includes =================================================================
+
+-include_lib("eunit/include/eunit.hrl").
+
+%%%_* Tests ====================================================================
+
+symbol_test_() ->
+  [?_assertEqual({ok, [token(atom, a, "a", {1, 1, 1}, {1, 1, 1})]},
+                 scan("a")),
+   ?_assertEqual({ok, [token(keyword, 'and', "and", {1, 1, 1}, {3, 1, 3})]},
+                 scan("and")),
+   ?_assertEqual({ok, [token(atom, 'fand', "fand", {1, 1, 1}, {4, 1, 4})]},
+                 scan("fand")),
+   ?_assertEqual({ok, [token(variable, 'Foo', "Foo", {1, 1, 1}, {3, 1, 3})]},
+                 scan("Foo")),
+   ?_assertEqual({error, {syntax_error, {{1, 1, 1}, "1foo"}}},
+                 scan("1foo"))
+  ].
+
+string_test_() ->
+  [?_assertEqual({ok, [token(atom, a, "'a'", {1, 1, 1}, {3, 1, 3})]},
+                 scan("'a'")),
+   ?_assertEqual({ok, [token(string, "a", "\"a\"", {1, 1, 1}, {3, 1, 3})]},
+                 scan("\"a\""))
+  ].
+
+integer_test_() ->
+  [?_assertEqual({ok, [token(integer, 16, "16", {1, 1, 1}, {2, 1, 2})]},
+                 scan("16")),
+   ?_assertEqual({ok, [token(integer, -16, "-16", {1, 1, 1}, {3, 1, 3})]},
+                 scan("-16")),
+   ?_assertEqual({ok, [token(integer, 22, "16#16", {1, 1, 1}, {5, 1, 5})]},
+                 scan("16#16")),
+   ?_assertEqual({ok, [token(integer, -22, "-16#16", {1, 1, 1}, {6, 1, 6})]},
+                 scan("-16#16")),
+   %% Note: The below is slighly different behaviour from the OTP compiler which
+   %% would give a bad_integer error.
+   ?_assertEqual({error, {syntax_error, {{3, 1, 3}, "#-16"}}},
+                 scan("16#-16")),
+   ?_assertEqual({error, {{illegal_base, 128}, {{1, 1, 1}, "128#16"}}},
+                 scan("128#16"))
+  ].
+
+float_test_() ->
+  [?_assertEqual({ok, [token(float, 16.15, "16.15", {1, 1, 1}, {5, 1, 5})]},
+                 scan("16.15")),
+   ?_assertEqual({ok, [token(float, 1.2e4, "1.2e4", {1, 1, 1}, {5, 1, 5})]},
+                 scan("1.2e4")),
+   ?_assertEqual({ok, [token(float, 1.2e-4, "1.2e-4", {1, 1, 1}, {6, 1, 6})]},
+                 scan("1.2e-4"))
+  ].
+
+character_test_() ->
+  [?_assertEqual({ok, [token(character, $a, "$a", {1, 1, 1}, {2, 1, 2})]},
+                 scan("$a")),
+   ?_assertEqual({ok, [token(character, $a, "$\\a", {1, 1, 1}, {3, 1, 3})]},
+                 scan("$\\a")),
+   ?_assertEqual({ok, [token(character, $a, "$\\a", {1, 1, 1}, {3, 1, 3})]},
+                 scan("$\\\a")),
+   ?_assertEqual({ok, [token(character, $\", "$\"", {1, 1, 1}, {2, 1, 2})]},
+                 scan("$\"")),
+   ?_assertEqual({ok, [token(character, $\n, "$\\n", {1, 1, 1}, {3, 1, 3})]},
+                 scan("$\\n")),
+   ?_assertEqual({ok, [token(character, $\^t, "$\\^t", {1, 1, 1}, {4, 1, 4})]},
+                 scan("$\\^t")),
+   ?_assertEqual({error, {syntax_error, {{1, 1, 1}, "$"}}},
+                 scan("$"))
+  ].
+
+
+%%%_* Test helpers =============================================================
+
+scan(Str) ->
+    parse_lib_scan:string(Str,
+                          plg_scan_erlang:grammar(),
+                          [{re_groups, named_only}]).
+
+token(Type,
+      Term,
+      Str,
+      {StartIndex, StartLine, StartCol},
+      {EndIndex, EndLine, EndCol}) ->
+    parse_lib_scan:token(Type,
+                         Term,
+                         Str,
+                         parse_lib_scan:point(StartIndex, StartLine, StartCol),
+                         parse_lib_scan:point(EndIndex, EndLine, EndCol)).
+
+%%%_* Emacs ====================================================================
+%%% Local Variables:
+%%% allout-layout: t
+%%% erlang-indent-level: 2
+%%% End:
